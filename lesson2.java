f12975b1a1e51513bd05a5131ae97e81bf5b493d
ronald-empire <144595521+ronald-empire@users.noreply.github.com>
2023-11-26 15:43:44 +0200

    Update lesson2.java

--- lesson2.java
+++ lesson2.java
@@ -67,7 +67,7 @@ public class Lesson2_Task4 {
 
 /// task 5 -
 import java.util.Scanner;
-public class task5_ronald {
+public class Lesson2_Task5 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.print("enter a string of 4 characters: ");
@@ -93,4 +93,30 @@ public class task5_ronald {
 }
 
 /// task 6 -
-
+import java.util.Random;
+public class Lesson2_Task6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int randomNumber = random.nextInt(99);
+        System.out.println(randomNumber);
+        if (randomNumber < 10) {
+            System.out.println("Number is single digit");
+        } 
+        else {
+            System.out.println("Number is double digit");
+        }
+        if (randomNumber % 2 == 0){
+            System.out.println("Number is divisible by two");
+        }
+        else {
+            System.out.println("Number does not divide by two");
+        }
+        if (randomNumber % 3 == 0){
+            System.out.println("Number is divisible by three");
+        }
+        else {
+            System.out.println("Number does not divide by three");
+        }
+    }
+}
