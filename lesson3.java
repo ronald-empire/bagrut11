ba022fad84b7cb460808e665fccda46383ff1d03
ronald-empire <144595521+ronald-empire@users.noreply.github.com>
2023-11-26 14:40:57 +0200

    שיעור 3

--- lesson3.java
+++ lesson3.java
@@ -1,3 +1,22 @@
+/// task 1 -
+import java.util.Scanner;
+class Lesson3_Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter a number: ");
+        int thenumber = scanner.nextInt();
+        if (thenumber < 0) {
+            System.out.println("number is *negative*");
+        }
+        if (thenumber > 0) {
+            System.out.println("number is *positive*");
+        }
+        if (thenumber == 0) {
+            System.out.println("number is zero");
+        }
+    }
+}
+/// task 2 -
 /// task 3 - calculate the average grade
 import java.util.Scanner;
 class Lesson3_Task3 {
