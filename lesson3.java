75c925da01e414eb0eb6711039ae8cfc3d6c8233
ronald-empire <144595521+ronald-empire@users.noreply.github.com>
2023-11-26 14:50:59 +0200

    Update lesson3.java

--- lesson3.java
+++ lesson3.java
@@ -81,4 +81,24 @@ class Lesson3_Task3 {
         System.out.println(average);
     }
 }
-/// task 5 - 
+/// task 5 - smallest number
+import java.util.Scanner;
+class Lesson3_Task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("first number: ");
+        int num1 = scanner.nextInt();
+        System.out.println("second number: ");
+        int num2 = scanner.nextInt();
+        System.out.println("third number: ");
+        int num3 = scanner.nextInt();
+        if (num1 < num2 && num1 < num3) {
+            System.out.println("The smallest number is: "+num1);
+        }
+        else if (num2 < num3) {
+            System.out.println("The smallest number is: "+num2);
+        }
+        else {
+            System.out.println(" The smallest number is: "+num3);
+        }
+    }
