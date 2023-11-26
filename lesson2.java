7fb30a197475a645c853e348ca3d994b0f688776
ronald-empire <144595521+ronald-empire@users.noreply.github.com>
2023-11-26 15:39:55 +0200

    Update lesson2.java

--- lesson2.java
+++ lesson2.java
@@ -1,4 +1,31 @@
 /// task 1 -
+import java.util.Random;
+public class Lesson2_Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int randomNumber = random.nextInt(99);
+        System.out.println(randomNumber);
+        if (randomNumber < 10) {
+            System.out.println("Number is single digit");
+        } 
+        else {
+            System.out.println("Number is double digit");
+        }
+        if (randomNumber % 2 == 0){
+            System.out.println("Number is divisible by two");
+        }
+        else {
+            System.out.println("Number does not divide by two");
+        }
+        if (randomNumber % 3 == 0){
+            System.out.println("Number is divisible by three");
+        }
+        else {
+            System.out.println("Number does not divide by three");
+        }
+    }
+}
 
 /// task 2 -
 
