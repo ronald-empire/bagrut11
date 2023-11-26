a03794f661baa895694e96870dcf0d9e7c1b5912
ronald-empire <144595521+ronald-empire@users.noreply.github.com>
2023-11-26 14:42:02 +0200

    Update lesson3.java

--- lesson3.java
+++ lesson3.java
@@ -16,7 +16,26 @@ class Lesson3_Task1 {
         }
     }
 }
-/// task 2 -
+/// task 2 - which number is biggest
+import java.util.Scanner;
+class Lesson3_Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter a number: ");
+        int num1 = scanner.nextInt();
+        System.out.println("enter a number: ");
+        int num2 = scanner.nextInt();
+        if (num1 > num2) {
+            System.out.println("first number is bigger");
+        }
+        if (num1 < num2) {
+            System.out.println("second number is bigger");
+        }
+        if (num1 == num2) {
+            System.out.println("nmubers are equal");
+        }
+    }
+}
 /// task 3 - calculate the average grade
 import java.util.Scanner;
 class Lesson3_Task3 {
