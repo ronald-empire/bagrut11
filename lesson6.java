7b0013957e701e95d089f45948305e7ac2aafd91
ronald-empire <144595521+ronald-empire@users.noreply.github.com>
2023-11-29 13:29:13 +0200

    Update lesson6.java

--- lesson6.java
+++ lesson6.java
@@ -1,8 +1,8 @@
 // RONALD GITLIN
 
-// task 1 - average 5 grades
+/// task 1 - average 5 grades
 import java.util.Scanner;
-public class Lesson5Task1 {
+public class Lesson6Task1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int amountGrades = 5, sumGrades = 0;
@@ -16,4 +16,20 @@ public class Lesson5Task1 {
     }
 }
 
-// task 2
+/// task 2 - break on 'enter'
+import java.util.Scanner;
+public class Lesson6Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int wordc = 0;
+        String input = "";
+        while (!input.equals("enter")) {
+            System.out.print("enter any word or 'enter' to finish: ");
+            input = scanner.nextLine();
+            wordc++;
+        }
+        System.out.print(wordc);
+    }
+}
+
+/// task 3 - 
