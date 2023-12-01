100aad603183e990f650cf110812abd8819529dd
ronald-empire <144595521+ronald-empire@users.noreply.github.com>
2023-12-01 21:41:57 +0200

    Update lesson6.java

--- lesson6.java
+++ lesson6.java
@@ -110,7 +110,15 @@ public class Lesson6Task5 {
 }
 
 /// task 6 - 
-
+public class Lesson6Task6 {
+    public static void main(String[] args) {
+        for (int i = 1; i<=10; i++) {
+            for (int j = 1; j<=10; j++) {
+                System.out.print("["+i*j+"] ");
+            }
+        }
+    }
+}
 
 /// task 7 - 
 
