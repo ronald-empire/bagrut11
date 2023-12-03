8b543c26b9fb8730e60ec9b2d7fe2fd570fed818
ronald-empire <144595521+ronald-empire@users.noreply.github.com>
2023-12-03 14:46:57 +0200

    תרגילים של שיעור 2

--- lesson2.java
+++ lesson2.java
@@ -1,1 +1,31 @@
+// RONALD GITLIN
 
+/// task 1 -
+class Lesson2_Task1 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 7;
+        System.out.println(a+"  "+b+"  "+c);
+    }
+}
+
+/// task 2 -
+class Lesson2_Task2 {
+    public static void main(String[] args) {
+        char alpha = 'g';
+        char beta = 'o';
+        char gamma = 'o';
+        char delta = 'd';
+        System.out.println(alpha+""+beta+""+gamma+""+delta);
+    }
+}
+
+/// task 3 - 
+class Lesson2_Task3 {
+    public static void main(String[] args) {
+        int n1 = 43;
+        int n2 = 22;
+        System.out.println(n1+""+n2);
+    }
+}
